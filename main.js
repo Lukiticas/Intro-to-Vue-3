d2c9190eb100b8accfbba99509dc03b21fd2dc46
Lucas Matheus Nunes Pereira <Lucasmathues416@outlook.com>
2023-01-09 20:23:34 -0300

    fourth lesson done

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
       product: "Socks",
       image: "./assets/images/socks_blue.jpg",
       inventory: 0,
+      onSale: this.inventory > 0,
     };
+  },
+  methods: {
+    onClick(descOrInc) {
+      if (descOrInc) {
+        this.inventory++;
+      } else {
+        this.inventory <= 0 ? null : (this.inventory -= 1);
+      }
+    },
   },
 });
