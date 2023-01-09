c00b7edce4cd01c378949b089e556d3fcf4ea77b
Lucas Matheus Nunes Pereira <Lucasmathues416@outlook.com>
2023-01-09 19:14:51 -0300

    fisrt lesson done

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      description:
+        " Lorem ipsum dolor sit, amet consectetur adipisicing elit. Similique esse quisquam rerum consectetur quae eius vel quas necessitatibus voluptate sed, aspernatur exercitationem mollitia culpa recusandae! Enim praesentium expedita nobis quas. ",
+    };
+  },
+});
